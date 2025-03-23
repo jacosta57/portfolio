6579a5331cd566cd873ebfac9bc9fb5d2128cdd1
Jayson Acosta <Jayson04@iastate.edu>
2025-03-23 00:56:44 -0500

    Working projects page without working links

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,24 @@
 import Navbar from '../components/Navbar/Navbar'
+import projects from '../assets/json/projects.json'
+import ProjectCard from '../components/Projects/ProjectCard';
 
-function Projects(){
-
-    return (
-        <>
-          <Navbar />
-        </>  
-      );
+function Projects() {
+  return (
+    <>
+      <Navbar />
+      <div className='container mt-5'>
+      <h2 className="mb-5 border-bottom border-primary pb-2 d-inline-block">All Projects</h2>
+        <div className='row'>
+          {Object.keys(projects).map((projectKey, index) => {
+            const project = projects[projectKey];
+            return (
+                <ProjectCard project={project} key={index}/>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Projects;
